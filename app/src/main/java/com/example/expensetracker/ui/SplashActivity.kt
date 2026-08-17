dfdfb70bf80460a79462c84a8f8b1bd2da00d969
StefanusDickyBayuSegara <bayusegara1011@gmail.com>
2026-08-17 16:09:15 +0700

    tambah splash screen dan custom app icon

---
+++ app/src/main/java/com/example/expensetracker/ui/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.expensetracker.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.expensetracker.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Tunggu 1.5 detik, terus pindah ke MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
